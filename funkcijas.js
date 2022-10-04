877d4b000bc93a525f1004a454c08f43d5887cf1
rkancevics <reinis.kancevics@gmail.com>
2022-10-04 09:36:15 +0300

    Es uzliku ūdeņradim, hēlijam un nātrijam, kad uzklikšķina uz viņiem, tad parādās teksts par tiem!

--- funkcijas.js
+++ funkcijas.js
@@ -2,3 +2,13 @@ function udenradis(){
     document.getElementById("teksts").innerHTML="Ūdeņradis ir labs"
     document.getElementById("bilde").src="./atteli/11561_Sun.png"
 }
+
+function helijs(){
+    document.getElementById("teksts").innerHTML="Hēlijs ir labs"
+    document.getElementById("bilde").src=""
+}
+
+function natrijs(){
+    document.getElementById("teksts").innerHTML="Nātrijs ir labs"
+    document.getElementById("bilde").src=""
+}
