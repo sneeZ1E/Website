0361e663dae2aa85d691c89b0742acdc1d775dd2
rkancevics <reinis.kancevics@gmail.com>
2022-10-04 15:45:18 +0300

    Izmainīju funkcijas.js magniju un alumīniju

--- funkcijas.js
+++ funkcijas.js
@@ -12,3 +12,13 @@ function natrijs(){
     document.getElementById("teksts").innerHTML="Nātrijs ir labs"
     document.getElementById("bilde").src=""
 }
+
+function magnijs(){
+    document.getElementById("teksts").innerHTML="Magnijs ir labs"
+    document.getElementById("bilde").src=""
+}
+
+function aluminijs(){
+    document.getElementById("teksts").innerHTML="Alumīnijs ir labs"
+   document.getElementById("bilde").src=""
+}
